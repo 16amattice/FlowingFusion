88d4eb63b407e3e06795e3fb88d3a2857fb25534
Anthony Mattice <AMattice@desert-aire.com>
2024-07-22 15:18:29 -0500

    Program.cs updates

--- FlowingFusion/Program.cs
+++ FlowingFusion/Program.cs
@@ -3,36 +3,23 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Listen on the port provided by Heroku
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-builder.WebHost.UseKestrel(serverOptions =>
-{
-    serverOptions.ListenAnyIP(int.Parse(port));
-});
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
 });
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseReDoc(c =>
 {
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseReDoc(c =>
-    {
-        c.SpecUrl = "/swagger/v1/swagger.json";
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SpecUrl = "/swagger/v1/swagger.json";
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
